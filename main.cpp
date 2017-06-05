b22657f8572217eee0daca23baa9a159919bb04f
danielalvarado <danialvarado@ic-itcr.ac.cr>
2017-06-05 11:08:05 -0600

    server

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 #include <fstream>
-//#include "listalugares.h"
 #include "arbolexpansion.h"
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <fstream>
+#include <strings.h>
+#include <stdlib.h>
+#include <string>
+#include <pthread.h>
+const int PRIMERA_VEZ = -1;
+const int OPCION_VENTA = 1;
+const int OPCION_PROVEEDOR_MAS_VENTAS = 2;
+const int OPCION_CLIENTE_MAS_COMPRO = 3;
+const int OPCION_PRODUCTO_MAS_VENDIDO = 4;
+const int OPCION_PRODUCTOS_CAMBIARON_STOCK = 5;
+const int OPCION_CATEGORIA_MAS_VENDIDA = 6;
+const int OPCION_SUPERMERCADO_MAS_VENTAS = 7;
+const int OPCION_LUGAR_CON_MAS_SUPERMERCADOS = 8;
+const int OPCION_IMPRIMIR_ARBOL_PREORDEN = 9;
+const int OPCION_ELIMINAR_PRODUCTO = 10;
+const int OPCION_ELIMINAR_CLIENTE = 11;
+const int OPCION_FACTURA = 12;
+int const TAMANHO_BUFFER = 256;
+
 void leerArchLugares(ListaLugares * _lugares){
     std::string nombreArchivo = "Lugares.txt";
     //std::string nombreArchivo = "LugaresDaniel.txt";
@@ -75,21 +103,120 @@ void leerArchConexiones(ListaLugares* _listaLugares) {
 
 }
 
+ListaLugares* listaLugares = new ListaLugares();
+ArbolExpansionMinimo* arbol = new ArbolExpansionMinimo();
+
+void *task1(void *);
+static int newsockfd;
+
+
+
 int main() {
 
-    ListaLugares* listaLugares = new ListaLugares();
     leerArchLugares(listaLugares);
     leerArchConexiones(listaLugares);
     //ArbolExpansionMinimo* arbolExpansionMinimo = new ArbolExpansionMinimo(_listaLugares);
     //int min = arbolExpansionMinimo->arbolExpancionPrim(_listaLugares);
     listaLugares->profundida(20);
 
-    ArbolExpansionMinimo* arbol = new ArbolExpansionMinimo();
+
     //arbol->prim(_listaLugares,78);
     arbol->prim(listaLugares,20);
+
+
+    int pId, portNo, listenFd;
+    socklen_t len; //store size of the address
+    bool loop = false;
+    struct sockaddr_in svrAdd, clntAdd;
+
+    pthread_t threadA[3];
+
+    portNo = 8888;
+
+    //create socket
+    listenFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if(listenFd < 0) {
+        std::cerr << "Cannot open socket" << std::endl;
+        return 0;
+    }
+
+    bzero((char*) &svrAdd, sizeof(svrAdd));
+
+    svrAdd.sin_family = AF_INET;
+    svrAdd.sin_addr.s_addr = INADDR_ANY;
+    svrAdd.sin_port = htons(portNo);
+
+    //bind socket
+    if(bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0) {
+        std::cerr << "Cannot bind" << std::endl;
+        return 0;
+    }
+
+    listen(listenFd, 5);
+
+
+
+    int noThread = 0;
+
+    while (noThread < 3) {
+
+
+
+        len = sizeof(clntAdd);
+        std::cout << "Listening" << std::endl;
+
+        //this is where client connects. svr will hang in this mode until client conn
+        newsockfd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
+
+        if (newsockfd < 0) {
+            std::cerr << "Cannot accept connection" << std::endl;
+            return 0;
+        }else {
+            std::cout << "Connection successful" << std::endl;
+        }
+
+        int neverUsedVariable;
+        pthread_create(&threadA[noThread], NULL, task1, NULL);
+
+        noThread++;
+    }
+
+    for(int i = 0; i < 3; i++) {
+        pthread_join(threadA[i], NULL);
+    }
+
 
 
 
 
     return 0;
+}
+
+void *task1 (void *dummyPt) {
+
+    std::cout << "Thread No: " << pthread_self() << std::endl;
+    char buffer[TAMANHO_BUFFER];
+    bzero(buffer, TAMANHO_BUFFER);
+    bool loop = false;
+    while(!loop) {
+        bzero(buffer, TAMANHO_BUFFER);
+
+
+        read(newsockfd, buffer, TAMANHO_BUFFER);
+        //write()
+
+
+        std::string tester (buffer);
+        std::cout << tester << std::endl;
+        char serverMsg[] = "BIENVENIDO AL SERVIDOR";
+        write(newsockfd,serverMsg,strlen(serverMsg));
+
+
+
+        if(tester == "exit")
+            break;
+    }
+    std::cout << "\nClosing thread and conn" << std::endl;
+    close(newsockfd);
 }
