0266ed8d373ff1903f6c9f757d9aca1310c0a447
rrsegnini <roberto.t.x@hotmail.com>
2017-06-07 22:04:02 -0600

    Server update

--- Serverprueba.cpp
+++ Serverprueba.cpp
@@ -62,8 +62,26 @@ int main()
         cout << "Client connected!" << endl;
         recv(client, buffer, sizeof(buffer), 0);
         cout<<buffer<<endl;
-    	string hola = "Hola";
+    	string hola = "Ingrese su codigo";
 		send(client, hola.c_str(), strlen(hola.c_str()), 0);
+		
+		recv(client, buffer, sizeof(buffer), 0);
+		
+		if (std::atoi(buffer) == 1){
+			hola = "CONECTADO";
+		send(client, hola.c_str(), strlen(hola.c_str()), 0);
+		}
+		memset(buffer, 0, sizeof(buffer));
+		
+		while (true){
+			memset(buffer, 0, sizeof(buffer));
+			recv(client, buffer, sizeof(buffer), 0);
+			cout<<buffer<<endl;
+			
+			
+			hola = "COSAS";
+		send(client, hola.c_str(), strlen(hola.c_str()), 0);
+		}
        //return 0;
     }
     closesocket(client);
