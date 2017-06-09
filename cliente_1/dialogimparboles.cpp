cb70c9f1f5654775e84ea716b3c64c3a05545e34
rrsegnini <roberto.t.x@hotmail.com>
2017-06-08 21:20:20 -0600

    Árboles cliente socket

--- cliente_1/dialogimparboles.cpp
+++ cliente_1/dialogimparboles.cpp
@@ -13,3 +13,111 @@ DialogImpArboles::~DialogImpArboles()
 {
     delete ui;
 }
+
+void DialogImpArboles::on_comboBox_activated(const QString &arg1)
+{
+
+}
+
+void DialogImpArboles::on_buttonBox_accepted()
+{
+    std::string itemCombo = ui->comboBox->currentText().toStdString();
+    if (itemCombo == "Proveedores") {
+        client->writeSocket("Proveedores");
+        client->readSocket();
+        std::string infoInSocket (client->buffer);
+        QString str = QString::fromUtf8(infoInSocket.c_str());
+        ui->plainTextEdit->insertPlainText(str);
+        ui->plainTextEdit->setReadOnly(true);
+    }
+
+    else if (itemCombo == "Categorias"){
+        client->writeSocket("Categorias");
+        client->readSocket();
+
+        std::string res = "";
+
+        string codL,codC,codS;
+        codL = ui->lineEdit_4->text().toStdString();
+        //codC = ui->lineEdit_2->text().toStdString();
+        codS = ui->lineEdit_3->text().toStdString();
+
+        res += codL + ";" + codS;
+
+        client->writeSocket(res);
+        client->readSocket();
+
+        std::string infoInSocket (client->buffer);
+        QString str = QString::fromUtf8(infoInSocket.c_str());
+        ui->plainTextEdit->insertPlainText(str);
+        ui->plainTextEdit->setReadOnly(true);
+    }
+
+    else if (itemCombo == "Productos"){
+            client->writeSocket("Productos");
+            client->readSocket();
+
+            std::string res = "";
+
+            string codL,codC,codS;
+            codL = ui->lineEdit_4->text().toStdString();
+            codC = ui->lineEdit_2->text().toStdString();
+            codS = ui->lineEdit_3->text().toStdString();
+
+            res += codL + ";" + codS + ";" + codC;
+
+            client->writeSocket(res);
+            client->readSocket();
+
+            std::string infoInSocket (client->buffer);
+            QString str = QString::fromUtf8(infoInSocket.c_str());
+            ui->plainTextEdit->insertPlainText(str);
+            ui->plainTextEdit->setReadOnly(true);
+        }
+
+    else if (itemCombo == "Clientes"){
+            client->writeSocket("Clientes");
+            client->readSocket();
+
+            //std::string res = "";
+
+           // string codL,codC,codS;
+            //codL = ui->lineEdit_4->text().toStdString();
+           // codC = ui->lineEdit_2->text().toStdString();
+            //codS = ui->lineEdit_3->text().toStdString();
+
+            res += codL + ";" + codS + ";" + codC; //No habria que mandar codigos
+            //pero no se si aca hace falta mandarle algo para que funciones el server
+
+            client->writeSocket(res);///////
+            client->readSocket();
+
+            std::string infoInSocket (client->buffer);
+            QString str = QString::fromUtf8(infoInSocket.c_str());
+            ui->plainTextEdit->insertPlainText(str);
+            ui->plainTextEdit->setReadOnly(true);
+        }
+
+    else if (itemCombo == "Supermercados"){
+            client->writeSocket("Supermercados");
+            client->readSocket();
+
+            std::string res = "";
+
+            string codL,codC,codS;
+            codL = ui->lineEdit_4->text().toStdString();
+           // codC = ui->lineEdit_2->text().toStdString();
+            //codS = ui->lineEdit_3->text().toStdString();
+
+            res += codL;
+
+            client->writeSocket(res);///////
+            client->readSocket();
+
+            std::string infoInSocket (client->buffer);
+            QString str = QString::fromUtf8(infoInSocket.c_str());
+            ui->plainTextEdit->insertPlainText(str);
+            ui->plainTextEdit->setReadOnly(true);
+        }
+
+}
